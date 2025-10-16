5412047f7066b7be386f5f379e29426a85cdc029
Thanh Sang <thanhsanggl06@gmail.com>
2025-10-17 04:16:59 +0700

    add c1 component

--- BlazorWasmHosted.Client/Program.cs
+++ BlazorWasmHosted.Client/Program.cs
@@ -1,11 +1,13 @@
+using BlazorWasmHosted.Client;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using BlazorWasmHosted.Client;
+using System.Net.NetworkInformation;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
 
 await builder.Build().RunAsync();
